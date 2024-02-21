80e08267a748b504d748b6d7c3dede4491fa5ffd
jfraught <fraughtdawg@gmail.com>
2024-02-21 09:05:46 -0700

    Initial Commit

---
+++ StormViewerUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StormViewerUIKit
+//
+//  Created by Jordan Fraughton on 2/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
